fcc6fd4144fff04b9b1691e496bf5dc5b4b8f2a3
Bo Sun <sunbo804400200@gmail.com>
2019-08-08 17:42:36 -0400

    wrote another file to fix the reshape problem

--- lane.py
+++ lane.py
@@ -2,8 +2,7 @@ import cv2
 import numpy as np
 
 def canny(image):
-    lane_image = np.copy(image)
-    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
@@ -14,17 +13,37 @@ def region_of_interest(image):
         [(200, height), (1100, height), (550, 250)]
     ])#get the polygon array
     mask = np.zeros_like(image)#make a black mask of the size of the image
-    cv2.fillPoly(mask, polygons, 128)
-    return mask
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
-    
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
+'''
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2=line.reshape(4)
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+            #its a BGR color, hence blue, 10 is the line thickness
+    return line_image
+'''
 image = cv2.imread('test_image.jpg')
-cropped_image = region_of_interest(canny(image))#get a image of the road(cropped for the road part)
+lane_image = np.copy(image)
+canny = canny(lane_image)
+cropped_image = region_of_interest(canny)#get a image of the road(cropped for the road part)
 #using hough transformation
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5),
 #2 as size of the 'bins', 100 as the threshold of the votes
 #a line has to atleast has 40 pixels and the gap between segments should not exceed 5 pixels
-cv2.imshow('result', region_of_interest(canny(image)))
+line_image = display_lines(lane_image, lines)
+cv2.imshow('result', line_image)
 cv2.waitKey(0)
 
 
---
+++ lane_detec.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, y1), (x2, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis = 0)
+    right_fit_average = np.average(right_fit, axis = 0)
+    
+def CannyEdge(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    cannyImage = cv2.Canny(blur, 50, 150)
+    return cannyImage
+
+def region_of_interest(image):
+    height = image.shape[0]
+    triangle = np.array([[(200, height),(550, 250),(1100, height),]], np.int32)
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, triangle, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
+
+lane_image = cv2.imread('test_image.jpg')
+canny = CannyEdge(lane_image)
+cropped_Image = region_of_interest(canny)
+rho = 2
+theta = np.pi/180
+threshold = 100
+lines = cv2.HoughLinesP(cropped_Image,rho, theta, threshold, np.array ([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('Lane Lines', combo_image)
+cv2.waitKey(0)
