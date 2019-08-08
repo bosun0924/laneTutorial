d70f5029819308708590c03e6987467c312604cf
Bo Sun <sunbo804400200@gmail.com>
2019-08-08 15:53:41 -0400

    Get the region of interest

--- lane.py
+++ lane.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 def canny(image):
     lane_image = np.copy(image)
@@ -9,10 +8,18 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([
+        [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
 image = cv2.imread('test_image.jpg')
-#cv2.imshow('result', canny(image))
-plt.imshow(canny(image))
-plt.show()
+cv2.imshow('result', region_of_interest(canny(image)))
+cv2.waitKey(0)
 
 
 
