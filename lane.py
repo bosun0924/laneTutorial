50e89e4219e42a9a1ce5a94ae543ce6a2b8cffa3
Bo Sun <sunbo804400200@gmail.com>
2019-08-08 16:05:14 -0400

    Excute Hough transformation to the image

--- lane.py
+++ lane.py
@@ -12,12 +12,18 @@ def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([
         [(200, height), (1100, height), (550, 250)]
-    ])
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, polygons, 255)
+    ])#get the polygon array
+    mask = np.zeros_like(image)#make a black mask of the size of the image
+    cv2.fillPoly(mask, polygons, 128)
     return mask
 
+    
 image = cv2.imread('test_image.jpg')
+cropped_image = region_of_interest(canny(image))#get a image of the road(cropped for the road part)
+#using hough transformation
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5),
+#2 as size of the 'bins', 100 as the threshold of the votes
+#a line has to atleast has 40 pixels and the gap between segments should not exceed 5 pixels
 cv2.imshow('result', region_of_interest(canny(image)))
 cv2.waitKey(0)
 
