f43272d7569be6a3b2b0292ff3034719a07772d2
Bo Sun <sunbo804400200@gmail.com>
2019-08-08 15:20:42 -0400

    Canny edge detector to extract edges

---
+++ lane.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def canny(image):
+    lane_image = np.copy(image)
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+image = cv2.imread('test_image.jpg')
+cv2.imshow('result', canny(image))
+cv2.waitKey(0)
+
+
+
